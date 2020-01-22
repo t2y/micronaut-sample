673af4d9c2701ea4b2bf66463a4e410b4a930baf
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2020-01-23 02:22:46 +0900

    update

--- src/main/java/micronaut/sample/Main.java
+++ src/main/java/micronaut/sample/Main.java
@@ -3,12 +3,12 @@
  */
 package micronaut.sample;
 
-public class App {
+public class Main {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new Main().getGreeting());
     }
 }
